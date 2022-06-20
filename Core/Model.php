11332c5223dfa700522fd4825c8d3a1fa2967df9
minhpro8vn <66260069+minhpro8vn@users.noreply.github.com>
2022-06-20 16:03:37 +0700

    Update Model.php

--- Core/Model.php
+++ Core/Model.php
@@ -17,7 +17,7 @@ abstract class Model
             $dsn = 'mysql:host=' . Config::DB_HOST . ';dbname=' . Config::DB_NAME . ';charset=utf8';
             $db = new PDO($dsn, Config::DB_USER, Config::DB_PASSWORD);
 
-            // Throw an Exception when an error occurs
+      
             $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         }
 
